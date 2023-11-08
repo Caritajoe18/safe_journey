b074cf4255408229c4965315c20c71f3c0f7d47c
CARITA NDIBE <139926895+Caritajoe18@users.noreply.github.com>
2023-11-08 21:30:47 +0100

    Update http.js

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL : "https://safe-journey-9lpi.vercel.app/"
+    baseURL : 
         //"http://localhost:5000/api",
-   // baseURL : "https://migos-server.onrender.com/api",
+   // baseURL : 
+        "https://migos-server.onrender.com/api",
     headers : {
         "Content-type" : "application/json"
     }
